6fb1a43b9d8a2693726d0a16f40f62269666fa5c
Harsh <harshvardhanbahree@gmail.com>
2024-08-19 18:04:47 +0530

    Learning typescripts

---
+++ learn.ts
@@ -0,0 +1,17 @@
+// let id : number = 5; 
+// let company : string= "Harsh";
+// let isPublished : boolean = false
+
+// let arr : number[] = [1,2];
+// let x: any = "blah blah";
+// let y: any= 1;
+// let anyArray: any[] = ["as",2,{}, true]
+
+
+
+const concateFunction =(a:string,b:string):string =>{
+    return a+b;
+}
+
+console.info(concateFunction("Hello","World"));
+console.info(concateFunction("1","2"));
